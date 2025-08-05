62764b8f493ba1737e977cd7ea989d2947beb0cf
Michael Bishop <cleverca22@gmail.com>
2025-08-05 16:47:31 -0300

    add pcsx-redux

--- flake.nix
+++ flake.nix
@@ -1,9 +1,12 @@
 {
   description = "PSn00bSDK flake for PS1 development";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    pcsx-redux.url = "github:cleverca22/pcsx-redux/fix-submodules";
+  };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, pcsx-redux }: {
     packages = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (system:
       let
         pkgsMipsel = import nixpkgs {
@@ -37,11 +40,20 @@
     );
 
     apps = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ] (system: let
-      pkgs = self.packages.${system};
+      self_packages = self.packages.${system};
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
       default = {
         type = "app";
-        program = "${pkgs.runDemo}/bin/runDemo";
+        program = "${self_packages.runDemo}/bin/runDemo";
+      };
+      pcsx-redux = let
+        script = pkgs.writeShellScript "pcsx-redux" ''
+          ${pcsx-redux.packages.${system}.default}/bin/pcsx-redux -loadexe ${self.packages.${system}.n00bdemo}/n00bdemo.exe -run
+        '';
+      in {
+        type = "app";
+        program = "${script}";
       };
     });
 
